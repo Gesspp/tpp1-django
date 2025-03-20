691892c0972bc8a0b707f95bcad7bd1c7e2e7ca0
Gesspp <nikitatum199@gmail.com>
2025-03-20 19:56:15 +0300

    все

---
+++ greetings/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('hello/<str:name>', views.hello, name='hello'),
+    path('stats/', views.stats, name='stats'),
+    path('ststs/<str:name>', views.ststs, name='ststs'),
+    path('', views.index, name='index'),
+]
---
+++ greetings/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Greeting
+
+
+def hello(request, name):
+    Greeting(name=name).save()
+    return render(request, 'hello.html', {'name': name})
+
+def stats(request):
+    count_greetings = Greeting.objects.count()
+    return render(request, 'stats.html', {'count_greetings': count_greetings})
+
+def ststs(request, name):
+    greetings = Greeting.objects.filter(name=name)
+    return render(request, 'ststs.html', {'greetings': greetings, 'name': name})
+
+def index(request):
+    return render(request, 'index.html')
